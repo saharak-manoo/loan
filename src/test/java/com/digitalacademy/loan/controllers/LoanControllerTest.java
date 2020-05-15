8ad8eb9e614cfc7cc771373ba7bf73955ea1793f
Saharak Manoo <Saharakmanoo@outlook.com>
2020-05-16 02:12:45 +0700

    [Updated] unit test

--- src/test/java/com/digitalacademy/loan/controllers/LoanControllerTest.java
+++ src/test/java/com/digitalacademy/loan/controllers/LoanControllerTest.java
@@ -10,6 +10,7 @@ import com.digitalacademy.loan.exceptions.LoanException;
 import com.digitalacademy.loan.models.LoanInfo;
 import com.digitalacademy.loan.services.LoanService;
 import static org.mockito.Matchers.any;
+import static org.mockito.Mockito.doReturn;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
@@ -18,21 +19,16 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.MockitoAnnotations;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.MvcResult;
-import org.springframework.test.web.servlet.ResultActions;
-import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.json.JSONObject;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -65,26 +61,18 @@ public class LoanControllerTest {
     loanInfo.setAccountReceivable("102-333-6666");
     loanInfo.setPrincipalAmount(2000.00);
 
-    when(loanServive.getloanInfoById(id)).thenReturn(loanInfo);
+    doReturn(loanInfo).when(loanServive).getloanInfoById(id);
 
-    MvcResult mvcResult = mvc.perform(get("/loan/info/{id}", id))
-                             .andExpect(status().isOk())
-                             .andReturn();
-                             
-    JSONObject resp = new JSONObject(mvcResult.getResponse().getContentAsString());
-    JSONObject status = new JSONObject(resp.getString("status"));
-    JSONObject data = new JSONObject(resp.getString("data"));
-
-    assertEquals("0", status.get("code").toString());
-    assertEquals("success", status.get("message").toString());
-
-    assertEquals(1, data.get("id"));
-    assertEquals("OK", data.get("status"));
-    assertEquals("102-444-6666", data.get("account_payable"));
-    assertEquals("102-333-6666", data.get("account_receivable"));
-    assertEquals(2000, data.get("principal_amount"));
-
-    verify(loanServive, times(1)).getloanInfoById(id);
+    mvc.perform(get("/loan/info/{id}", id))
+       .andExpect(status().isOk())
+       .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+       .andExpect(jsonPath("$.status.code").value("0"))
+       .andExpect(jsonPath("$.status.message").value("success"))
+       .andExpect(jsonPath("$.data.id").value(1))
+       .andExpect(jsonPath("$.data.status").value("OK"))
+       .andExpect(jsonPath("$.data.account_payable").value("102-444-6666"))
+       .andExpect(jsonPath("$.data.account_receivable").value("102-333-6666"))
+       .andExpect(jsonPath("$.data.principal_amount").value(2000));
   }
 
   @DisplayName("Test get loan by id 2 and throws error")
@@ -96,15 +84,11 @@ public class LoanControllerTest {
       new LoanException(LoanError.GET_LOAN_NOT_FOUND, HttpStatus.BAD_REQUEST)
     );
 
-    MvcResult mvcResult = mvc.perform(get("/loan/info/{id}", id))
-                             .andExpect(status().isBadRequest())
-                             .andReturn();
-
-    JSONObject resp = new JSONObject(mvcResult.getResponse().getContentAsString());
-    JSONObject status = new JSONObject(resp.getString("status"));
-
-    assertEquals("LOAN4002", status.get("code").toString());
-    assertEquals("Loan infomation not found", status.get("message").toString());
+    mvc.perform(get("/loan/info/{id}", id))
+       .andExpect(status().isBadRequest())
+       .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+       .andExpect(jsonPath("$.status.code").value("LOAN4002"))
+       .andExpect(jsonPath("$.status.message").value("Loan infomation not found"));
   }
 
   @DisplayName("Test get loan by id 3 and throws error")
@@ -118,14 +102,8 @@ public class LoanControllerTest {
 
     mvc.perform(get("/loan/info/{id}", id))
        .andExpect(status().isInternalServerError())
-       .andExpect(content().contentType("application/json"))
+       .andExpect(content().contentType(MediaType.APPLICATION_JSON))
        .andExpect(jsonPath("$.status.code").value("LOAN4001"))
        .andExpect(jsonPath("$.status.message").value("Cannot get loan infomation"));
-
-    // JSONObject resp = new JSONObject(mvcResult.getResponse().getContentAsString());
-    // JSONObject status = new JSONObject(resp.getString("status"));
-
-    // assertEquals("LOAN4001", status.get("code").toString());
-    // assertEquals("Cannot get loan infomation", status.get("message").toString());
   }
 }
